ca9b01cb08d97635f92e9abfbb866b514515798f
AmusingUsername <jrmtaylor@gmail.com>
2016-04-02 00:03:18 -0400

    Initial commit for StringHelper

---
+++ Helper/StringHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Underlines the specified string with a line of same character length.
+        /// </summary>
+        /// <param name="stringToUnderline">String to underline</param>
+        /// <param name="underlineChar">Character used for underlining the passed string</param>
+        /// <returns>Underlined string</returns>
+        public static string underlineString(this string stringToUnderline, char underlineChar = '-')
+        {
+            string retVal = stringToUnderline + Environment.NewLine;
+            for(int i = 0; i < stringToUnderline.Length; i++)
+            {
+                retVal += underlineChar;
+            }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Splits a CSV string into items and trims whitespace.
+        /// </summary>
+        /// <param name="CSV">CSV string</param>
+        /// <returns>CSV items in string array</returns>
+        public static string[] csvToStringArray(string CSV)
+        {
+            string[] retVal = CSV.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i < retVal.Length; i++) { retVal[i] = retVal[i].Trim(); }
+            return retVal;
+        }
+    }
+}
